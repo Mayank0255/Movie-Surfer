a57127ea20b2c9668e23e28342792bf3ef32c375
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-06-14 02:19:37 +0530

    style: formatting of colons and quotes

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require("express"),
+const express = require('express'),
     app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    User = require("./models/user"),
-    flash = require("connect-flash"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local");
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    User = require('./models/user'),
+    flash = require('connect-flash'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local');
 
-mongoose.connect("mongodb://localhost/MovieSurfer", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(flash());
 
 // PASSPORT CONFIGURATION
 
-app.use(require("express-session")({
-    secret: "SYSTEM BREACHED !!!",
+app.use(require('express-session')({
+    secret: 'SYSTEM BREACHED !!!',
     resave: false,
     saveUninitialized: false
 }));
@@ -32,19 +32,16 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
     next();
 });
 
-app.use(require("./server/routes/index"));
-app.use(require("./server/routes/discover"));
-app.use(require("./server/routes/show"));
-app.use(require("./server/routes/auth"));
+app.use(require('./server/routes/index'));
+app.use(require('./server/routes/discover'));
+app.use(require('./server/routes/show'));
+app.use(require('./server/routes/auth'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 1000;
 
-app.listen(1000, function() {
-    console.log("MovieSurfer server started");
-
-});
+app.listen(port, () => console.log(`Running at ${port}`));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const User = require("../../models/user");
+const User = require('../../models/user');
 const passport = require('passport');
 
 
-router.get("/register", function(req, res) {
-    res.render("register");
+router.get('/register', function(req, res) {
+    res.render('register');
 });
 
-router.post("/register", function(req, res) {
+router.post('/register', function(req, res) {
     User.register(new User({ firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, username: req.body.username }), req.body.password, function(err, user) {
         if (err) {
             console.log(err);
-            return res.redirect("/register");
+            return res.redirect('/register');
         }
-        passport.authenticate("local")(req, res, function() {
-            res.redirect("/index");
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/index');
         });
     });
 });
@@ -24,22 +24,22 @@ router.post("/register", function(req, res) {
 
 // LOGIN ROUTES
 
-router.get("/login", function(req, res) {
-    res.render("login");
+router.get('/login', function(req, res) {
+    res.render('login');
 });
 
-router.post("/login", function(req, res, next) {
-    passport.authenticate("local", {
-        successRedirect: "/index",
-        failureRedirect: "/login"
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', {
+        successRedirect: '/index',
+        failureRedirect: '/login'
     })(req, res);
 });
 
 // LOGOUT ROUTES
 
-router.get("/logout", function(req, res) {
+router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect("/index");
+    res.redirect('/index');
 });
 
 module.exports = router;
