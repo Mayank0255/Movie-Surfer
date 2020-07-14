f6181686c930725e731e4f108c50c65cb66740b3
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-15 01:44:26 +0530

    feat: apply es6 concepts

--- server/routes/discover.js
+++ server/routes/discover.js
@@ -3,26 +3,26 @@ const router = express.Router();
 const request = require('request');
 const { API_URL, API_KEY_URL } = require('../constants/index');
 
-router.get('/index/discover/:genre_name/:genre_id/:page', function(req, res) {
+router.get('/index/discover/:genre_name/:genre_id/:page', (req, res) => {
     const genre_id = req.params.genre_id;
     const genre_name = req.params.genre_name;
     const page = req.params.page;
     request(API_URL + `/discover/movie${API_KEY_URL}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genre_id}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('discover', { data: data, genre_name: genre_name, genre_id: genre_id, page: page });
             }
         });
 });
 
-router.get('/indextv/discover/:genre_name/:genre_id/:page', function(req, res) {
+router.get('/indextv/discover/:genre_name/:genre_id/:page', (req, res) => {
     const genre_id = req.params.genre_id;
     const genre_name = req.params.genre_name;
     const page = req.params.page;
     request(API_URL + `/discover/tv${API_KEY_URL}&sort_by=popularity.desc&page=${page}&timezone=America%2FNew_York&with_genres=${genre_id}&include_null_first_air_dates=false`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('discovertv', { data: data, genre_name: genre_name, genre_id: genre_id, page: page });
             }
@@ -32,11 +32,11 @@ router.get('/indextv/discover/:genre_name/:genre_id/:page', function(req, res) {
 // ====================================================== MOST POPULAR AND TOP RATED =====================================================
 
 // MOST POPULAR MOVIES
-router.get('/index/discover/pop/:page', function(req, res) {
+router.get('/index/discover/pop/:page', (req, res) => {
     const page = req.params.page;
     request(API_URL + `/movie/popular${API_KEY_URL}&page=${page}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('popmovie', { data: data, page: page });
             }
@@ -44,11 +44,11 @@ router.get('/index/discover/pop/:page', function(req, res) {
 });
 
 // TOP RATED MOVIES
-router.get('/index/discover/top/:page', function(req, res) {
+router.get('/index/discover/top/:page', (req, res) => {
     const page = req.params.page;
     request(API_URL + `/movie/top_rated${API_KEY_URL}&page=${page}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('topmovie', { data: data, page: page });
             }
@@ -57,11 +57,11 @@ router.get('/index/discover/top/:page', function(req, res) {
 
 // MOST POPULAR TV SHOWS
 
-router.get('/indextv/discover/pop/:page', function(req, res) {
+router.get('/indextv/discover/pop/:page', (req, res) => {
     const page = req.params.page;
     request(API_URL + `/tv/popular${API_KEY_URL}&page=${page}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('poptv', { data: data, page: page });
             }
@@ -70,11 +70,11 @@ router.get('/indextv/discover/pop/:page', function(req, res) {
 
 // TOP RATED TV SHOWS
 
-router.get('/indextv/discover/top/:page', function(req, res) {
+router.get('/indextv/discover/top/:page', (req, res) => {
     const page = req.params.page;
     request(API_URL + `/tv/top_rated${API_KEY_URL}&page=${page}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('toptv', { data: data, page: page });
             }
@@ -84,12 +84,12 @@ router.get('/indextv/discover/top/:page', function(req, res) {
 
 // ======================================================= SEARCH PAGE ===================================================================
 
-router.get('/index/search/:page', function(req, res) {
+router.get('/index/search/:page', (req, res) => {
     const query = req.query.search;
     const page = req.params.page;
     request(API_URL + `/search/multi${API_KEY_URL}&query=${query}&page=${page}&include_adult=false`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('search', { data: data, query: query, page: page });
             }
--- server/routes/show.js
+++ server/routes/show.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const request = require('request');
 const { API_URL, API_KEY_URL } = require('../constants/index');
 
-router.get('/indextv/discover/:tv_id/show', function(req, res) {
+router.get('/indextv/discover/:tv_id/show', (req, res) => {
     const tv_id = req.params.tv_id;
 
     request(API_URL + `/tv/${tv_id}${API_KEY_URL}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('showtv', { data: data, tv_id: tv_id });
             }
@@ -16,19 +16,19 @@ router.get('/indextv/discover/:tv_id/show', function(req, res) {
 });
 
 
-router.get('/index/discover/:movie_id/show', function(req, res) {
+router.get('/index/discover/:movie_id/show', (req, res) => {
     const movie_id = req.params.movie_id;
 
     request(API_URL + `/movie/${movie_id}${API_KEY_URL}`,
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+        (error, response, body) => {
+            if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body);
                 res.render('show', { data: data, movie_id: movie_id });
             }
         });
 });
 
-router.get('/watchlist', function(req, res) {
+router.get('/watchlist', (req, res) => {
     res.render('watchlist');
 });
 
