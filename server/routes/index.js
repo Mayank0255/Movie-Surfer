d62a2ded5e1e0518e5abf1b090bb4154be0b2431
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-15 01:44:26 +0530

    feat: apply es6 concepts

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,22 +4,22 @@ const request = require('request');
 const imageConfig = require('../../imageConfig');
 const { API_URL, MOVIE_GENRE_URL, TV_GENRE_URL } = require('../constants/index');
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     res.render('landing');
 });
 
-router.get('/index', function(req, res) {
-    request(API_URL + MOVIE_GENRE_URL, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+router.get('/index', (req, res) => {
+    request(API_URL + MOVIE_GENRE_URL, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
             const genreNames = JSON.parse(body);
             res.render('index', { genreNames: genreNames, images: imageConfig.images });
         }
     });
 });
 
-router.get('/indextv', function(req, res) {
-    request(API_URL + TV_GENRE_URL, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+router.get('/indextv', (req, res) => {
+    request(API_URL + TV_GENRE_URL, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
             const genreNames = JSON.parse(body);
             res.render('indextv', { genreNames: genreNames, imagesTv: imageConfig.imagesTv });
         }
