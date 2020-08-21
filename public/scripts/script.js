d689b4c597784f09988d92a1af79a800420297f2
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-22 01:47:50 +0530

    fix: linting errors

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,32 +1,21 @@
-function hoverpanelin(id) {
-    var parent = document.getElementById(id)
+const hoverPanelIn = id => {
+    const parent = document.getElementById(id)
     parent.style.backgroundColor = '#00c8ff'
-    var child = parent.getElementsByTagName('div')[0]
-    var child2 = parent.getElementsByTagName('div')[1]
-    child.style.backgroundColor = '#001d26'
-        // child.style.borderRadius = '4px'
-    child2.style.backgroundColor = '#001d26'
-
-
+    const children = parent.getElementsByTagName('div')
+    children[0].style.backgroundColor = '#001d26'
+    children[1].style.backgroundColor = '#001d26'
 }
 
-function hoverpanelout(id) {
-    var parent = document.getElementById(id)
-    parent.style.backgroundColor = null
-    var child = parent.getElementsByTagName('div')[1]
+const hoverPanelOut = id => document.getElementById(id).style.backgroundColor = null;
+
+const hoverGenreIn = id => {
+    const element = document.getElementById(id);
+    element.getElementsByTagName('img')[0].style.border = '3px solid #00c8ff';
+    element.getElementsByClassName('link')[0].getElementsByTagName('a')[0].style.color = '#00c8ff';
 }
 
-
-function hovergenrein(id) {
-    console.log(id);
-    document.getElementById(id).getElementsByTagName('img')[0].style.border = "3px solid #00c8ff";
-    document.getElementById(id).getElementsByClassName('link')[0].getElementsByTagName('a')[0].style.color = "#00c8ff";
-    // document.getElementsByClassName("img-thumbnail").style.border = "1px solid #00c8ff";
-}
-
-function hovergenreout(id) {
-    console.log(id);
-    document.getElementById(id).getElementsByTagName('img')[0].style.border = null;
-    document.getElementById(id).getElementsByClassName('link')[0].getElementsByTagName('a')[0].style.color = "#ffffff";
-    // document.getElementsByClassName("img-thumbnail").style.border = "1px solid #00c8ff";
+const hoverGenreOut = id => {
+    const element = document.getElementById(id);
+    element.getElementsByTagName('img')[0].style.border = null;
+    element.getElementsByClassName('link')[0].getElementsByTagName('a')[0].style.color = '#ffffff';
 }
